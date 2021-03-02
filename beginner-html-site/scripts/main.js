ff1cc8ddd5d576b9322f9a0266aa811c063a59bc
caohuijun2018 <1160364324@qq.com>
2021-03-02 17:15:29 +0800

    添加个性欢迎信息

--- beginner-html-site/scripts/main.js
+++ beginner-html-site/scripts/main.js
@@ -8,12 +8,17 @@ myImage.onclick = ( () => {
     }
 })
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('hi')
+let myHeading = document.querySelector('h1')
 
 function setUsername() {
     let myName = prompt('请输入你的名字');  //弹出对话框，要求用户输入数据
-    localStorage.setItem('name',myName);    // localStorage API ，它可以将数据存储在浏览器中供后续获取
-    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    if(!myName || myName === null){
+        setUsername()
+    }else{
+        localStorage.setItem('name',myName);    // localStorage API ，它可以将数据存储在浏览器中供后续获取
+        myHeading.innerHTML = 'Mozilla 酷毙了，' + myName;
+    }
+   
     
 }
 
